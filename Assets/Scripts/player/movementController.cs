65beb9a6612b78c5b9fea72855ed3a04fc7e4306
Henry Oliver <henry983615@gmail.com>
2020-03-16 14:37:01 +1300

    movement

--- Assets/Scripts/player/movementController.cs
+++ Assets/Scripts/player/movementController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class movementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float moveSpeed = 10.0f;
+    public float jumpForce = 10.0f;
+    public float gravity = 9.41f;
+    public float feetradius = 0.5f;
+    public LayerMask groundLayer;
+    public Transform feet;
+
+    private CharacterController ch;
+    private bool isOnGround = true;
+    private Vector3 moveDir = Vector3.zero;
+
+    private void Start()
     {
-        
+        ch = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        isOnGround = Physics.CheckSphere(feet.position, feetradius, groundLayer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (isOnGround)
+        {
+
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * moveSpeed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDir.y += jumpForce;
+            }
+        }
+
+        //Apply Gravity
+        moveDir.y -= gravity * Time.deltaTime;
+
+        //Move
+        ch.Move(moveDir * Time.deltaTime);
     }
 }
