145ac34b30a61de920071110f637f800876798a0
Vaughan Webb <vaughanw2012@gmail.com>
2020-03-17 13:46:38 +1300

    ghoopoosststttt

---
+++ Assets/Scripts/boss/ghostEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostEffect : MonoBehaviour
+{
+    public GameObject particles;
+    public Mesh theMesh;
+
+    void Start()
+    {
+        GameObject tmp = Instantiate(particles, gameObject.transform);
+
+        //tmp.GetComponent<ParticleSystem>().shape.mesh = theMesh;
+
+
+        var sh = tmp.GetComponent<ParticleSystem>().shape;
+        sh.mesh = theMesh;
+    }
+
+}
