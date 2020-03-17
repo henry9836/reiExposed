b77057b7d22ea6e0577b3416038a0623e4313b61
Vaughan Webb <vaughanw2012@gmail.com>
2020-03-17 16:49:42 +1300

    ghost camera works

--- Assets/Scripts/boss/ghostEffect.cs
+++ Assets/Scripts/boss/ghostEffect.cs
@@ -5,17 +5,57 @@ using UnityEngine;
 public class ghostEffect : MonoBehaviour
 {
     public GameObject particles;
-    public Mesh theMesh;
+    //public Mesh theMesh;
+
+    public List<GameObject> body = new List<GameObject>() { };
+    public List<Mesh> meshes = new List<Mesh>() { };
+    public List<GameObject> ghostbody = new List<GameObject>() { };
+
+
 
     void Start()
     {
-        GameObject tmp = Instantiate(particles, gameObject.transform);
+        for (int i = 0; i < this.gameObject.transform.childCount; i++)
+        {
+            if (this.gameObject.transform.GetChild(i).tag == "body")
+            {
+                body.Add(this.gameObject.transform.GetChild(i).gameObject);
+            }
+        }
+
+        for (int i = 0; i < body.Count; i++)
+        {
+            meshes.Add(body[i].GetComponent<SkinnedMeshRenderer>().sharedMesh);
+
+        }
+
+        for (int i = 0; i < body.Count; i++)
+        {
+            GameObject tmp = Instantiate(particles, body[i].gameObject.transform);
+
+            tmp.transform.parent = body[i].GetComponent<SkinnedMeshRenderer>().rootBone;
+            var sh = tmp.GetComponent<ParticleSystem>().shape;
+            sh.mesh = meshes[i];
+
+            ghostbody.Add(tmp);
+            tmp.AddComponent<MeshCollider>();
+            tmp.GetComponent<MeshCollider>().convex = true;
+            tmp.GetComponent<MeshCollider>().sharedMesh = meshes[i];
+
+            tmp.name = tmp.name + i;
+            
+
+
+            body[i].GetComponent<SkinnedMeshRenderer>().sharedMesh = null;
+
+        }
+
+
+
 
         //tmp.GetComponent<ParticleSystem>().shape.mesh = theMesh;
 
 
-        var sh = tmp.GetComponent<ParticleSystem>().shape;
-        sh.mesh = theMesh;
     }
 
 }
