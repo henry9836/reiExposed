c086348b19a2a6e93ce3e1c25e62a61887cb3211
Henry Oliver <henry983615@gmail.com>
2020-04-02 21:04:41 +1300

    hey some are in

--- Assets/Scripts/boss/idleDemoBoss.cs
+++ Assets/Scripts/boss/idleDemoBoss.cs
@@ -11,16 +11,16 @@ public class idleDemoBoss : StateMachineBehaviour
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
         
-    }
+    //}
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
         
-    }
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
