e7f070ab8804316b51f339774b914104c284f38f
Vaughan Webb <vaughanw2012@gmail.com>
2020-04-27 22:15:13 +1200

    update usability

---
+++ Assets/Scripts/UI/newUI/settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class settings : MonoBehaviour
+{
+    saveFile save;
+
+    void Start()
+    {
+        float volume = 0.69f;
+        save = GameObject.Find("save").GetComponent<saveFile>();  
+        
+        save.saveitem("volume", volume);
+
+        Debug.Log(save.safeItem("volume", saveFile.types.FLOAT).tofloat);
+
+
+    }
+
+
+}
