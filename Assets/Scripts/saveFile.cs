87403a02885ba8975bf02aafaa94bf0fcf927c59
Vaughan Webb <vaughanw2012@gmail.com>
2020-04-27 17:39:23 +1200

    save file works

--- Assets/Scripts/saveFile.cs
+++ Assets/Scripts/saveFile.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.Rendering.HighDefinition;
+
+
 
 public class saveFile : MonoBehaviour
 {
@@ -15,30 +18,63 @@ public class saveFile : MonoBehaviour
         public string data;
         public int dataline;
 
-        public filedump(bool mexists, string mname, string mdata, int mdataline)
+        public string[] allData;
+
+        public filedump(bool mexists, string mname, string mdata, int mdataline, string[] mallData)
         {
             this.exists = mexists;
             this.name = mname;
             this.data = mdata;
             this.dataline = mdataline;
+            this.allData = mallData;
         }
     }
 
+    public struct interpret
+    {
+        public int toint;
+        public float tofloat;
+        public string tostring;
+    }
+
+    public enum types
+    { 
+        INT,
+        STRING,
+        FLOAT,
+    };
+
     void Start()
     {
-        Debug.Log(saveFilePath);
-
-
         righter = new StreamWriter(saveFilePath, true);
         righter.Close();
 
-        saveitem("test", "zoop");
-        Readitem("test");
+        int no1 = 420;
+        string no2 = "meme";
+        float no3 = 69.9999f;
 
+        saveitem("int", no1);
+        saveitem("string", no2);
+        saveitem("float", no3);
+
+        //int test1 = safeItem("int", types.INT).toint; //returns int
+        //string test2 = safeItem("string", types.STRING).tostring; //returns string
+        //int test3 = safeItem("float", types.INT).toint; //error cannot covert float to int
     }
 
 
-    public bool saveitem(string name, string item)
+    public void saveitem(string name, int item)
+    {
+        saveitem(name, item.ToString());
+    }
+
+    public void saveitem(string name, float item)
+    {
+        saveitem(name, item.ToString());
+    }
+
+
+    public void saveitem(string name, string item)
     {
         string input1 = "[" + name + "]" + "\n" + item;
         string input2 = item;
@@ -54,17 +90,84 @@ public class saveFile : MonoBehaviour
         }
         else
         {
-            //righter.write
-            Debug.Log("already exist");
+            if (info.data != item)
+            {
+                righter = new StreamWriter(saveFilePath, false);
+
+                Debug.Log(info.allData);
+
+                string toinsert = "";
+
+                for (int i = 0; i < info.allData.Length; i++)
+                {
+                    if (i == info.dataline)
+                    {
+                        toinsert = toinsert + input2 + "\n";
+                    }
+                    else
+                    {
+                        toinsert = toinsert + info.allData[i] + "\n";
+                    }
+                }
+
+                righter.Write(toinsert);
+
+                righter.Close();
+
+                Debug.Log("overwritten");
+            }
+            else
+            {
+                Debug.Log("already exists");
+            }
+        }
+    }
+
+    public interpret safeItem(string name, types type)
+    {
+        interpret test = GetItem(name);
+
+        if (type == types.INT)
+        {
+            if (test.toint == -999999)
+            {
+                Debug.LogError("Failed to save >" + name +  "< of type " + type);
+            }
+        }
+        else if (type == types.FLOAT)
+        {
+            if (test.tofloat == -999999f)
+            {
+                Debug.LogError("Failed to save >" + name + "< of type " + type);
+            }
         }
 
-        return true;
+        return (test);
+    }
+
+    public interpret GetItem(string name)
+    {
+        filedump tmp = Readitem(name);
+
+        interpret ans = new interpret();
+
+        if (int.TryParse(tmp.data, out ans.toint) == false)
+        {
+            ans.toint = -999999;
+        }
+        if (float.TryParse(tmp.data, out ans.tofloat) == false)
+        {
+            ans.tofloat = -999999f;
+        }
+
+        ans.tostring = tmp.data;
+        return (ans);
     }
 
     public filedump Readitem(string name)
     {
         string[] tmp = File.ReadAllLines(saveFilePath);
-        int save = 9999;
+        int save = 999999;
 
         for (int i = 0; i < tmp.Length; i++)
         {
@@ -74,14 +177,13 @@ public class saveFile : MonoBehaviour
             }
         }
 
-        if (save != 9999)
+        if (save != 999999)
         {
             righter.Close();
 
-            filedump exist = new filedump(true, name, tmp[save + 1], save + 1);
+            filedump exist = new filedump(true, name, tmp[save + 1], save + 1, tmp);
 
-
-            return exist;
+            return (exist);
         }
 
 
