ece79d54616356ce94f9202ae899382bf8a2bb1c
Vaughan Webb <vaughanw2012@gmail.com>
2020-05-11 15:11:35 +1200

    konbini progress

---
+++ Assets/Scripts/UI/newUI/currency.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class currency
+{
+    public static int Followers = 1000;
+    public static int MythTraces = 0;
+    public static int credits = 0;
+
+    public static int tengu = 0;
+    public static int kappa = 0;
+    public static int oni = 0;
+
+
+}
