7e5396785d1cdafa1a5a4163ec24e12c084b4995
Vaughan Webb <vaughanw2012@gmail.com>
2020-06-08 20:12:11 +1200

    pressing l takes screenshots and folders it

--- Assets/Scripts/UI/newUI/phone/ThePhone.cs
+++ Assets/Scripts/UI/newUI/phone/ThePhone.cs
@@ -1,27 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Data.SqlClient;
 
 public class ThePhone : MonoBehaviour
 {
     private plugindemo drone;
-    public GameObject thephone;
+    public GameObject ThePhoneUI;
     public GameObject rei;
     public GameObject phonecam;
+    public GameObject canvas;
 
-
+    private bool sucess;
 
     void Start()
     {
-        thephone = GameObject.Find("phone");
+        ThePhoneUI = GameObject.Find("phone");
         rei = GameObject.FindGameObjectWithTag("Player");
+        canvas = this.gameObject;
 
         //drone = GameObject.Find("Save&Dronemanage").GetComponent<plugindemo>();
 
         //if (drone.candeliver == true)
         //{
-        //    //drone.deliver();
-
+        //    drone.deliver();
         //}
     }
 
@@ -31,25 +34,33 @@ public class ThePhone : MonoBehaviour
          {
             openingephone(true);
          }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            takepicture();
+        }
     }
 
     public void openingephone(bool open)
     {
         if (open == true)
         {
-            thephone.SetActive(true);
+            ThePhoneUI.SetActive(true);
             rei.transform.GetChild(0).gameObject.SetActive(false);
             phonecam.SetActive(true);
             Time.timeScale = 0.0f;
-            //disbale and enable mesh rendere
+            //disbale mesh rendere
+            //disable all other UI
         }
         else
         {
-            thephone.SetActive(false);
+            ThePhoneUI.SetActive(false);
             rei.transform.GetChild(0).gameObject.SetActive(true);
             phonecam.SetActive(false);
             Time.timeScale = 1.0f;
-            //disbale and enable mesh rendere
+            //enable mesh rendere
+            //enable all other UI
+
 
         }
     }
@@ -58,16 +69,18 @@ public class ThePhone : MonoBehaviour
     { 
     
     }
+
     public void cameraroll()
     { 
     
     }
+
     public void thecamera()
     {
-        thephone.SetActive(false);
+        ThePhoneUI.SetActive(false);
         Time.timeScale = 1.0f;
-
     }
+
     public void amazon()
     { 
     
@@ -78,4 +91,58 @@ public class ThePhone : MonoBehaviour
         Time.timeScale = 0.0f;
     }
 
+    public void takepicture()
+    {
+        StartCoroutine(photo());
+        Debug.Log(sucess);
+
+    }
+
+    public IEnumerator photo()
+    {
+        List<GameObject> reenable = new List<GameObject>() { };
+
+        for (int i = 0; i < this.gameObject.transform.childCount; i++)
+        {
+            if (this.transform.GetChild(i).gameObject.activeSelf == true)
+            {
+                reenable.Add(this.transform.GetChild(i).gameObject);
+                this.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+
+        string foldername = "shhhhhSecretFolder/";
+        sucess = false; 
+
+        if (!Directory.Exists(foldername))
+        {
+            Directory.CreateDirectory(foldername);
+        }
+
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (!FileExists(foldername + i.ToString() + ".png"))
+            {
+                sucess = true;
+                ScreenCapture.CaptureScreenshot(foldername + i.ToString() + ".png");
+                i = 10;
+            }
+
+            yield return null;
+        }
+
+        for (int i = 0; i < reenable.Count; i++)
+        {
+            reenable[i].SetActive(true);
+        }
+
+        yield return null;
+    }
+
+    public bool FileExists(string path)
+    {
+        return File.Exists(path);
+    }
 }
