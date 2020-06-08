15cf0e7814e0a3c08c1e6af150921a5a740af0cb
Vaughan Webb <vaughanw2012@gmail.com>
2020-06-08 16:13:56 +1200

    Auto stash before merge of "phone" and "master"

--- Assets/Scripts/UI/newUI/phone/ThePhone.cs
+++ Assets/Scripts/UI/newUI/phone/ThePhone.cs
@@ -5,22 +5,76 @@ using UnityEngine;
 public class ThePhone : MonoBehaviour
 {
     private plugindemo drone;
+    public GameObject thephone;
+    public GameObject rei;
+    public GameObject phonecam;
+
+
 
     void Start()
     {
-        drone = GameObject.Find("Save&Dronemanage").GetComponent<plugindemo>();
+        thephone = GameObject.Find("phone");
+        rei = GameObject.FindGameObjectWithTag("Player");
+
+        //drone = GameObject.Find("Save&Dronemanage").GetComponent<plugindemo>();
+
+        //if (drone.candeliver == true)
+        //{
+        //    //drone.deliver();
+
+        //}
     }
 
     void Update()
     {
+         if (Input.GetKeyDown(KeyCode.Tab) == true)
+         {
+            openingephone(true);
+         }
+    }
 
-        if (drone.candeliver == true)
+    public void openingephone(bool open)
+    {
+        if (open == true)
         {
-            if (Input.GetKeyDown(KeyCode.Tab) == true)
-            {
-                drone.deliver();
-            }
+            thephone.SetActive(true);
+            rei.GetComponent<movementController>().enabled = false;
+            rei.transform.GetChild(0).gameObject.SetActive(false);
+            phonecam.SetActive(true);
+            Time.timeScale = 0.0f;
         }
+        else
+        {
+            thephone.SetActive(false);
+            rei.GetComponent<movementController>().enabled = true;
+            rei.transform.GetChild(0).gameObject.SetActive(true);
+            phonecam.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public void clues()
+    { 
+    
+    }
+    public void cameraroll()
+    { 
+    
+    }
+    public void thecamera()
+    {
+        Time.timeScale = 1.0f;
+
+        //GameObject.Find
+    }
+    public void amazon()
+    { 
+    
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 0.0f;
     }
 
 }
