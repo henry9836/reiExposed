a8e6cead7d873afe200c4084862b4e0c9d62a12a
Henry Oliver <henry983615@gmail.com>
2020-06-10 13:07:41 +1200

    vfx in but lags asking artist for special model

---
+++ Assets/Scripts/ApplyNewInfoVFX.cs
@@ -0,0 +1,32 @@
+﻿using Smrvfx;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class ApplyNewInfoVFX : MonoBehaviour
+{
+    public static readonly string PosMap = "PositionMap";
+    public static readonly string VelMap = "VelocityMap";
+
+    public int count;
+
+    private SkinnedMeshBaker sb;
+    private VisualEffect vfx;
+
+    private void Start()
+    {
+        sb = GetComponent<SkinnedMeshBaker>();
+        vfx = GetComponent<VisualEffect>();
+    }
+
+    void FixedUpdate()
+    {
+        if (count < 60) {
+            GetComponent<VisualEffect>().SetTexture(PosMap, sb.PositionMap);
+            GetComponent<VisualEffect>().SetTexture(VelMap, sb.VelocityMap);
+            count++;
+            Debug.Log("updated");
+        }
+    }
+}
---
+++ Assets/Scripts/boss/VFXGhostEffectBuilder.cs
@@ -0,0 +1,46 @@
+﻿using Smrvfx;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+public class VFXGhostEffectBuilder : MonoBehaviour
+{
+
+    public GameObject template;
+    public GameObject VFXGroupFolder;
+    public bool Build = false;
+
+    public static readonly string PosMap = "PositionMap";
+    public static readonly string VelMap = "VelocityMap";
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Build)
+        {
+            GameObject[] bodys = GameObject.FindGameObjectsWithTag("body");
+
+            for (int i = 0; i < bodys.Length; i++)
+            {
+
+                //if ((i % 4) == 0) {
+
+                GameObject tmp = Instantiate(template, Vector3.zero, Quaternion.identity);
+                tmp.transform.parent = VFXGroupFolder.transform;
+                tmp.AddComponent<SkinnedMeshBaker>();
+                tmp.GetComponent<SkinnedMeshBaker>()._source = bodys[i].GetComponent<SkinnedMeshRenderer>();
+
+                tmp.GetComponent<VisualEffect>().SetTexture(PosMap, tmp.GetComponent<SkinnedMeshBaker>().PositionMap);
+                tmp.GetComponent<VisualEffect>().SetTexture(VelMap, tmp.GetComponent<SkinnedMeshBaker>().VelocityMap);
+
+                //}
+
+                bodys[i].GetComponent<SkinnedMeshRenderer>().enabled = false;
+            }
+
+        }
+
+    }
+}
