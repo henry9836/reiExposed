c63398e1f899a1f02b5786ebe11faecc5624ab71
Henry Oliver <henry983615@gmail.com>
2020-07-01 16:48:59 +1200

    switched form cs to cpp due to platform restrctions

---
+++ Assets/Scripts/Networking/ServerSide/ServerController.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <strings.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+
+	return 0;
+}
