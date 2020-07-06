90142f9422d66488ced48509adfeeb8287e4bde0
Vaughan Webb <vaughanw2012@gmail.com>
2020-07-06 14:39:06 +1200

    ui basic for networking implmented into gameplay

---
+++ Assets/Scripts/Networking/messagepickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class messagepickup : MonoBehaviour
+{
+    private GameObject canvas;
+
+    void Start()
+    {
+        canvas = GameObject.FindGameObjectWithTag("MainCanvas");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canvas.GetComponent<enemydrop>().enemyiskil();
+            Destroy(this.gameObject); 
+        }
+    }
+}
