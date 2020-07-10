a182cf187ca144a8c236591c69b650b3c9e23d62
Vaughan Webb <vaughanw2012@gmail.com>
2020-07-10 16:22:16 +1200

    ping

---
+++ Assets/Scripts/UI/newUI/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Markup;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    [Range(0.0f, 10.0f)]
+    public float mouseSence;
+    public cameraControler CC;
+
+    private GameObject settingsmousesencetext;
+    private GameObject settingsmousesenceslider;
+
+
+
+    void Start()
+    {
+        settingsmousesenceslider = this.gameObject.transform.GetChild(1).GetChild(1).gameObject;
+        settingsmousesencetext = this.gameObject.transform.GetChild(1).GetChild(2).GetChild(1).gameObject;
+    }
+
+    public void apply()
+    {
+        mouseSence = float.Parse(settingsmousesencetext.GetComponent<Text>().text);
+        Debug.Log($"In mouse: {mouseSence} and we turned it into {Mathf.Clamp(mouseSence, 0.0f, 10.0f)}");
+        mouseSence = Mathf.Clamp(mouseSence, 0.0f, 10.0f);
+        CC.mouseSensitivity = mouseSence;
+        settingsmousesencetext.GetComponent<Text>().text = mouseSence.ToString("F2");
+        settingsmousesenceslider.GetComponent<Slider>().value = mouseSence;
+    }
+
+}
--- Assets/Scripts/player/materialDither.cs
+++ Assets/Scripts/player/materialDither.cs
@@ -20,7 +20,7 @@ public class materialDither : MonoBehaviour
     void Update()
     {
         float dist = Vector3.Distance(cam.position, player.position);
-        Debug.Log(dist);
+        //Debug.Log(dist);
         reimat.SetFloat("Vector1_A8AC28E2", dist);
         gunMat.SetFloat("Vector1_A8AC28E2", dist);
     }
