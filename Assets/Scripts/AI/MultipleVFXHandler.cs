cdb560d0d38871e81d2bf4e6145f441472a6f7fd
Henry Oliver <henry983615@gmail.com>
2020-07-16 18:14:32 +1200

    tweaks and unicode buffer size adjustment

--- Assets/Scripts/AI/MultipleVFXHandler.cs
+++ Assets/Scripts/AI/MultipleVFXHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MultipleVFXHandler : MonoBehaviour
 {
-    List<ParticleSystem> particles = new List<ParticleSystem>();
+    public List<ParticleSystem> particles = new List<ParticleSystem>();
 
     public void Play(int i)
     {
