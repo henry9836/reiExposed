ccfe80cf365ee7a5436ac667cfc7d92be62d7c7b
Henry Oliver <henry983615@gmail.com>
2020-07-22 13:46:27 +1200

    boop

---
+++ Assets/Scripts/UI/newUI/logs/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logger : MonoBehaviour
+{
+    public class LogContainer
+    {
+        public string msg = "ERROR";
+        public bool important = false;
+
+        public LogContainer(string m)
+        {
+            msg = m;
+            important = false;
+        }
+        public LogContainer(string m, bool i)
+        {
+            msg = m;
+            important = i;
+        }
+    }
+
+    public List<LogContainer> logs = new List<LogContainer>();
+
+    void AddNewMessage(LogContainer log)
+    {
+        logs.Add(log);
+    }
+
+
+
+
+    private void Start()
+    {
+        //Testing
+        for (int i = 0; i < 15; i++)
+        {
+            AddNewMessage(new LogContainer(Random.Range(0, 9999).ToString(), false));
+            AddNewMessage(new LogContainer(Random.Range(0, 9999).ToString(), true));
+        }
+
+    }
+
+}
