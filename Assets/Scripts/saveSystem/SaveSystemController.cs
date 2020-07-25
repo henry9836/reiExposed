b181f6b31fee10c3d7daa3e3e1d6a9e4c7b51999
Henry Oliver <henry983615@gmail.com>
2020-07-26 06:32:51 +1200

    save system is working

--- Assets/Scripts/saveSystem/SaveSystemController.cs
+++ Assets/Scripts/saveSystem/SaveSystemController.cs
@@ -7,11 +7,18 @@ public static class SaveSystemController
 {
 
     private const string saveFile = "rei.sav";
+    private const string IDFLAG = "#{ID}#";
+    private const string VALFLAG = "#{VAL}#";
 
     public class entry
     {
         public string id = "Untitled";
         public string value = "-1";
+
+        public entry(string _id)
+        {
+            id = _id;
+        }
 
         public entry(string _id, string _val)
         {
@@ -24,29 +31,91 @@ public static class SaveSystemController
     public static List<entry> saveInfomation = new List<entry>();
     public static bool ioBusy = false; //Used for telling user not to alt-f4
 
+    //Loads data from savefile into saveInfomation
+    public static void loadDataFromDisk() { loadDataFromDisk(saveFile); } 
+    //Loads data from savefile into saveInfomation
+    public static void loadDataFromDisk(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            //If not busy
+            if (!ioBusy)
+            {
+                //Set busy bit
+                ioBusy = true;
+
+                //Read all lines into array
+                string[] lines = File.ReadAllLines(filePath);
+
+                //Decode
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    //Is there a flag?
+                    if (lines[i].Contains(IDFLAG))
+                    {
+                        //Add id
+                        saveInfomation.Add(new entry(lines[i].Substring(IDFLAG.Length)));
+                    }
+                    else if (lines[i].Contains(VALFLAG))
+                    {
+                        //Set value of latest seen entry
+                        saveInfomation[saveInfomation.Count - 1].value = lines[i].Substring(VALFLAG.Length);
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"Unknown Line {lines[i]}");
+                    }
+                }
+
+                //Unset busy bit
+                ioBusy = false;
+            }
+            else
+            {
+                Debug.LogError("Cannot read file as it is busy");
+            }
+        }
+    }
+
     //Saves current state of saveInfomation to save file
     public static void saveDataToDisk() { saveDataToDisk(saveFile); }
     //Saves current state of saveInfomation to save file
     public static void saveDataToDisk(string filePath)
     {
-        //Set busy bit
-        ioBusy = true;
-
-        //Create saveFile if it doesn't exist and Open for writing
-        StreamWriter writer = new StreamWriter(filePath, false);
-
-        //For each entry in our save infomation overwrite file
-        for (int i = 0; i < saveInfomation.Count; i++)
+        //If not busy
+        if (!ioBusy)
         {
-            writer.WriteLine(saveInfomation[i]);
+            //Set busy bit
+            ioBusy = true;
+
+            //Create saveFile if it doesn't exist and Open for writing
+            StreamWriter writer = new StreamWriter(filePath, false);
+
+            //For each entry in our save infomation overwrite file
+            for (int i = 0; i < saveInfomation.Count; i++)
+            {
+                writer.WriteLine(IDFLAG + saveInfomation[i].id);
+                writer.WriteLine(VALFLAG + saveInfomation[i].value);
+            }
+
+            //Close writer
+            writer.Close();
+
+            //Unset busy bit
+            ioBusy = false;
         }
-
-        //Close writer
-        writer.Close();
-
-        //Unset busy bit
-        ioBusy = false;
+        else
+        {
+            Debug.LogError("Cannot read file as it is busy");
+        }
     }
+
+    //Update a value in our saveInfomation
+    public static void updateValue(string _id, bool _newValue) { updateValue(_id, _newValue.ToString()); }
+    //Update a value in our saveInfomation
+    public static void updateValue(string _id, float _newValue) { updateValue(_id, _newValue.ToString()); }
+    //Update a value in our saveInfomation
+    public static void updateValue(string _id, int _newValue) { updateValue(_id, _newValue.ToString()); }
 
     //Update a value in our saveInfomation
     public static void updateValue(string _id, string _newValue)
@@ -81,5 +150,43 @@ public static class SaveSystemController
         return "-1";
     }
 
-
+    //Load data as int type
+    public static int getIntValue(string _id)
+    {
+        int result = -1;
+        if (int.TryParse(getValue(_id), out result))
+        {
+            return result;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    //Load data as float type
+    public static float getFloatValue(string _id)
+    {
+        float result = -1.0f;
+        if (float.TryParse(getValue(_id), out result))
+        {
+            return result;
+        }
+        else
+        {
+            return -1.0f;
+        }
+    }
+    //Load data as bool type
+    public static bool getBoolValue(string _id)
+    {
+        bool result = false;
+        if (bool.TryParse(getValue(_id), out result))
+        {
+            return result;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
---
+++ Assets/Scripts/saveSystem/SaveSystemInvoker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSystemInvoker : MonoBehaviour
+{
+    private void Start()
+    {
+        //Load this first for things to work
+        SaveSystemController.loadDataFromDisk();
+
+        //Get Values
+        float one = SaveSystemController.getFloatValue("hahahahah");
+        float two = SaveSystemController.getFloatValue("funny funny");
+        one += 1.2f;
+        float three = one * two;
+
+        //Update Info
+        SaveSystemController.updateValue("hahahahah", one);
+        SaveSystemController.updateValue("funny funny", two);
+        SaveSystemController.updateValue("funny funny123", three);
+
+
+
+        //Save
+        StartCoroutine(delayed());
+    }
+
+    IEnumerator delayed()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SaveSystemController.saveDataToDisk();
+        Debug.Log("File written to!");
+    }
+
+}
