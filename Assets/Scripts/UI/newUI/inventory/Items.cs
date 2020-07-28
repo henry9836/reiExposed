7427cadacff37c4ca96e2b63783acf020e048c2d
Henry <henry983615@gmail.com>
2020-07-29 08:23:18 +1200

    powerup effects should work

--- Assets/Scripts/UI/newUI/inventory/Items.cs
+++ Assets/Scripts/UI/newUI/inventory/Items.cs
@@ -63,9 +63,13 @@ public class Items : MonoBehaviour
     public int biginvinsize = 50;
     public int equpiiedsize = 8;
 
+    PlayerController player;
+    movementController movement;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        movement = player.GetComponent<movementController>();
         loaditems();
         ////////////////////demo/////////////////////
         //for (int i = 0; i < 52; i++)
@@ -186,6 +190,107 @@ public class Items : MonoBehaviour
         removeitem(equipped[equippedpos], useitem);
     }
 
+    /*
+     * =====================
+     * ITEM EFFECTS SECTION
+     * =====================
+     */
+
+    //Changes health based on a percent of the health
+    void HealthEffector(float percent)
+    {
+        float amount = player.maxHealth * percent;
+        player.EffectHeatlh(amount);
+    }
+
+    //Applies a random effect
+    void DuckBehaviour()
+    {
+        switch (Random.Range(0, 4))
+        {
+            case 1: //Random Health Effect
+                {
+                    HealthEffector(Random.Range(-0.15f, 0.15f));
+                    break;
+                }
+            case 2: //Random Damage Applier
+                {
+                    StartCoroutine(ApplyTimedEffect(AllItems.DAMAGEBUFF, Random.Range(-0.25f, 0.25f), Random.Range(3.0f, 6.0f)));
+                    break;
+                }
+            case 3: //Random Stamina Applier
+                {
+                    StartCoroutine(ApplyTimedEffect(AllItems.STAMINABUFF, Random.Range(-0.25f, 0.25f), Random.Range(3.0f, 6.0f)));
+                    break;
+                }
+            case 4: //Movement Stamina Applier
+                {
+                    StartCoroutine(ApplyTimedEffect(AllItems.MOVEBUFF, Random.Range(-0.25f, 0.25f), Random.Range(3.0f, 6.0f)));
+                    break;
+                }
+            default:
+                {
+                    Debug.LogWarning("No Duck Behaviour Set up");
+                    break;
+                }
+        }
+    }
+
+    //Timed effects
+    IEnumerator ApplyTimedEffect(AllItems item, float percentToChange, float amountOfTimeToApply)
+    {
+        switch (item)
+        {
+            case AllItems.DAMAGEBUFF:
+                {
+                    //Calc
+                    float before = player.umbreallaDmg;
+                    float result = before * percentToChange;
+                    //Apply
+                    player.umbreallaDmg += result;
+                    //Wait
+                    yield return new WaitForSeconds(amountOfTimeToApply);
+                    //Unapply
+                    player.umbreallaDmg -= result;
+                    break;
+                }
+            case AllItems.STAMINABUFF:
+                {
+                    //Calc
+                    float before = player.staminaRegenSpeed;
+                    float result = before * percentToChange;
+                    //Apply
+                    player.staminaRegenSpeed += result;
+                    //Wait
+                    yield return new WaitForSeconds(amountOfTimeToApply);
+                    //Unapply
+                    player.staminaRegenSpeed -= result;
+                    break;
+                }
+            case AllItems.MOVEBUFF:
+                {
+                    //Calc
+                    float before = movement.moveSpeed;
+                    float result = before * percentToChange;
+                    //Apply
+                    movement.moveSpeed += result;
+                    //Wait
+                    yield return new WaitForSeconds(amountOfTimeToApply);
+                    //Unapply
+                    movement.moveSpeed -= result;
+                    break;
+                }
+            default:
+                {
+                    Debug.LogWarning($"No timed behaviour for item: {item}");
+                    break;
+                }
+        }
+
+        yield return null;
+    }
+
+    //Use an item
     private void removeitem(singleItem toremove, bool useitem)
     {
         if (useitem == true)
@@ -198,41 +303,49 @@ public class Items : MonoBehaviour
                 case AllItems.HEALTHDEBUFF_SMALL:
                     {
                         //hurt a bit
+                        HealthEffector(-0.05f);
                         break;
                     }
                 case AllItems.HEALTHBUFF:
                     {
                         //heal a lot
+                        HealthEffector(0.25f);
                         break;
                     }
                 case AllItems.HEALTHBUFF_SMALL:
                     {
                         //heal
+                        HealthEffector(0.05f);
                         break;
                     }
                 case AllItems.DAMAGEBUFF:
                     {
                         //Higher damage for time
+                        StartCoroutine(ApplyTimedEffect(AllItems.DAMAGEBUFF, 0.15f, 5.0f));
                         break;
                     }
                 case AllItems.STAMINABUFF:
                     {
                         //Regen faster stamina for time
+                        StartCoroutine(ApplyTimedEffect(AllItems.STAMINABUFF, 0.15f, 5.0f));
                         break;
                     }
                 case AllItems.MOVEBUFF:
                     {
                         //Faster movement for time
+                        StartCoroutine(ApplyTimedEffect(AllItems.MOVEBUFF, 0.15f, 5.0f));
                         break;
                     }
                 case AllItems.MOVEDEBUFF:
                     {
                         //Slower movement for time
+                        StartCoroutine(ApplyTimedEffect(AllItems.MOVEBUFF, -0.15f, 5.0f));
                         break;
                     }
                 case AllItems.DUCK:
                     {
                         //Random effect
+                        DuckBehaviour();
                         break;
                     }
                 default:
@@ -243,6 +356,12 @@ public class Items : MonoBehaviour
             }
 
         }
+
+        /*
+         * ========================
+         * ITEM EFFECTS SECTION END
+         * ========================
+         */
 
         if (toremove.equipped == true)
         {
