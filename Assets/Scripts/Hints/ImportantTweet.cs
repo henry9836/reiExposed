71baeaa144ac2e05f5eddbff7d22620ddaf65c26
Henry Oliver <henry983615@gmail.com>
2020-08-03 15:02:11 +1200

    waiting for v

---
+++ Assets/Scripts/Hints/ImportantTweet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImportantTweet : MonoBehaviour
+{
+
+    public Logger logger;
+    public enemydrop dropControl;
+
+    public string hint = "Important Hint";
+    public Items.AllItems item1 = Items.AllItems.NONE;
+    public Items.AllItems item2 = Items.AllItems.NONE;
+    public Items.AllItems item3 = Items.AllItems.NONE;
+    public bool hintImportant = true;
+
+    private float currency = 100.0f;
+
+    private void Start()
+    {
+
+        currency = Random.Range(100.0f, 300.0f);
+
+        if (!logger)
+        {
+            logger = GameObject.Find("MessageLog").GetComponent<Logger>();
+        }
+        if (!dropControl)
+        {
+            dropControl = GameObject.Find("Canvas").GetComponent<enemydrop>();
+        }
+    }
+
+    public void triggerTweet()
+    {
+        dropControl
+        logger.AddNewMessage(new Logger.LogContainer(hint, hintImportant));
+    }
+
+}
