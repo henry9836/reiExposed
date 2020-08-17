009fd35af8edd8f0dc42551d8f3231abbf39adc1
Vaughan Webb <vaughanw2012@gmail.com>
2020-08-17 15:45:58 +1200

    boss works in build REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
    REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Assets/Materials/vaughan/boss/drawTest.cs
+++ Assets/Materials/vaughan/boss/drawTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class drawTest : MonoBehaviour
 {
@@ -13,11 +14,13 @@ public class drawTest : MonoBehaviour
 
     private RaycastHit hit;
     public LayerMask tohit;
+
+    public Vector4 topass;
     void Start()
     {
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         toMat = new Material(shader);
-        toMat.SetVector("_Color", Color.red);
+        toMat.SetColor("_Color", Color.red);
 
         fromMat = GetComponent<SkinnedMeshRenderer>().material;
         splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
@@ -27,23 +30,24 @@ public class drawTest : MonoBehaviour
     void Update()
     {
         Debug.DrawRay(cam.transform.position, cam.transform.forward);
+
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            if (Physics.Raycast(cam.transform.position, cam.transform.forward,  out hit, Mathf.Infinity, tohit))
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, Mathf.Infinity, tohit))
             {
-                toMat.SetVector("_Coordinate", new Vector4(hit.textureCoord.x, hit.textureCoord.y, 0, 0));
+                topass = new Vector4(hit.textureCoord.x, hit.textureCoord.y, 0 , 0);
+                toMat.SetVector("_Coordinate", topass); //_Coordinate
                 RenderTexture tmp = RenderTexture.GetTemporary(splatmap.width, splatmap.height, 0, RenderTextureFormat.ARGBFloat);
                 Graphics.Blit(splatmap, tmp);
                 Graphics.Blit(tmp, splatmap, toMat);
                 RenderTexture.ReleaseTemporary(tmp);
-
                 fromMat.SetTexture("Texture2D_DB299D9F", splatmap);
             }
         }
     }
 
-    private void OnGUI()
-    {
-        GUI.DrawTexture(new Rect(0, 0, 256, 256), splatmap, ScaleMode.ScaleToFit, false, 1);
-    }
+    //private void OnGUI()
+    //{
+    //    GUI.DrawTexture(new Rect(0, 0, 256, 256), splatmap, ScaleMode.ScaleToFit, false, 1);
+    //}
 }
