00adec1a778e322f3da9756dd8efcd765344c2f7
henry9836 <henry983615@gmail.com>
2020-08-18 17:07:42 +1200

    they move now

--- Assets/Scripts/AILegacy/Myth/Animator Behaviour/EnemyBlock.cs
+++ Assets/Scripts/AILegacy/Myth/Animator Behaviour/EnemyBlock.cs
@@ -7,7 +7,7 @@ public class EnemyBlock : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.GetComponent<EnemyController>().stopMovement();
+        animator.gameObject.GetComponent<AIMovement>().stopMovement();
         animator.SetBool("Blocking", true);
         animator.ResetTrigger("Block");
     }
