84b456eaef91ba7228155929298623a5eff9d474
Vaughan Webb <vaughanw2012@gmail.com>
2020-08-24 22:53:21 +1200

    stupid animator is stupid
    and dumb booo

---
+++ Assets/Scripts/UI/konbini/camMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class camMove : MonoBehaviour
+{
+    public Animator animationref;
+
+    public enum locations
+    { 
+        ITEM,
+        UMBRELLA,
+        STORAGE,
+    }
+
+    public void move(locations from, locations to)
+    {
+        if (from == locations.ITEM)
+        {
+            if (to == locations.STORAGE)
+            {
+                animationref.SetFloat("dir", -1.0f);
+                animationref.SetInteger("current", 0);
+            }
+            else if (to == locations.UMBRELLA)
+            {
+                animationref.SetFloat("dir", 1.0f);
+                animationref.SetInteger("current", 1);
+            }
+        }
+        else if (from == locations.UMBRELLA)
+        {
+            if (to == locations.ITEM)
+            {
+                animationref.SetFloat("dir", -1.0f);
+                animationref.SetInteger("current", 1);
+            }
+            else if (to == locations.STORAGE)
+            {
+                animationref.SetFloat("dir", 1.0f);
+                animationref.SetInteger("current", 2);
+            }
+        }
+        else if (from == locations.STORAGE)
+        {
+            if (to == locations.ITEM)
+            {
+                animationref.SetFloat("dir", 1.0f);
+                animationref.SetInteger("current", 0);
+            }
+            else if (to == locations.UMBRELLA)
+            {
+                animationref.SetFloat("dir", -1.0f);
+                animationref.SetInteger("current", 2);
+            }
+        }
+
+
+    }
+}
