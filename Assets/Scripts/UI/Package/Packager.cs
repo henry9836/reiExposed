16fa926cfdebead11d36da5ded2305ebf7f0fd35
henry9836 <henry983615@gmail.com>
2020-09-20 12:25:41 +1200

    ultimate timer mega 4000 and 2

--- Assets/Scripts/UI/Package/Packager.cs
+++ Assets/Scripts/UI/Package/Packager.cs
@@ -33,12 +33,14 @@ public class Packager : MonoBehaviour
                                 "0x",
                                 "\\" };
 
+    float levelTime = 3600.0f;
     Items items;
     Color originalMessageColor;
     Color originalCurrencyColor;
 
     private void Start()
     {
+        levelTime = Time.timeSinceLevelLoad;
         Cursor.visible = true;
         originalMessageColor = message.color;
         originalCurrencyColor = currency.color;
@@ -87,7 +89,10 @@ public class Packager : MonoBehaviour
     private void FixedUpdate()
     {
 
-
+        if (Input.GetKeyDown(KeyCode.Semicolon))
+        {
+            Debug.Log(levelTime);
+        }
 
         if (Cursor.lockState != CursorLockMode.Confined || !Cursor.visible)
         {
@@ -203,7 +208,7 @@ public class Packager : MonoBehaviour
         sender.dditem2 = (int)item2;
         sender.dditem3 = (int)item3;
         sender.ddname = nameField.text;
-        sender.ddtime = NetworkUtility.convertToTime(Time.timeSinceLevelLoad);
+        sender.ddtime = NetworkUtility.convertToTime(levelTime);
 
         //Remove Items
         items.removeitemequipped(item1, false);
