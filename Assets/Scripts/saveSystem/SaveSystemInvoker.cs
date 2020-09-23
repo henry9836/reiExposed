19cc159a29e9eb1c7c12f0107970290ae2311e1a
henry9836 <henry983615@gmail.com>
2020-09-23 22:22:56 +1200

    was not allowed into game becuase of hash change

--- Assets/Scripts/saveSystem/SaveSystemInvoker.cs
+++ Assets/Scripts/saveSystem/SaveSystemInvoker.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class SaveSystemInvoker : MonoBehaviour
 {
+
+    bool checkedHash = false;
+
     private void Awake()
     {
         //Load this first for things to work
         SaveSystemController.loadDataFromDisk();
     }
 
-#if UNITY_EDITOR
     private void FixedUpdate()
     {
+#if UNITY_EDITOR
         if (Input.GetKeyDown(KeyCode.Semicolon))
         {
             Debug.Log(SaveSystemController.checkSaveValid());
@@ -23,6 +27,19 @@ public class SaveSystemInvoker : MonoBehaviour
                 SaveSystemController.Reset();
             }
         }
-    }
 #endif
+
+        //Ready to interface with and on the main menu
+        if (SaveSystemController.loadedValues && !checkedHash && (SceneManager.GetActiveScene().buildIndex == 0))
+        {
+            //Check Hash
+            if (!SaveSystemController.checkSaveValid())
+            {
+                //CHEATS!!!!
+                Debug.LogError("CHEATER DETECTED!!!");
+                SaveSystemController.Reset();
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
 }
