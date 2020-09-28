a137493c63eb449ffbd113622fb75804a17b0f9c
Vaughan Webb <vaughanw2012@gmail.com>
2020-09-28 16:28:21 +1300

    gamma can be adjusted

---
+++ Assets/Scripts/UI/pregame/gammaAdjust.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class gammaAdjust : MonoBehaviour
+{
+    public Volume post;
+    public void updateValue()
+    {        
+        LiftGammaGain tmp;
+        if (post.profile.TryGet(out tmp))
+        { 
+            tmp.gamma.value = new Vector4(0.0f, 0.0f, 0.0f, this.GetComponent<Slider>().value);
+        }
+    }
+}
