aa5d2ef440a3907ee40d491e890513446c809249
Vaughan Webb <vaughanw2012@gmail.com>
2020-10-01 16:59:16 +1300

    poke for henry and credits progress

---
+++ Assets/Scripts/UI/credits/mouseCredit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mouseCredit : MonoBehaviour
+{
+    public Canvas parentCanvas;
+    public Image mouseCursor;
+
+    public void Start()
+    {
+        Cursor.visible = false;
+    }
+
+
+    public void Update()
+    {
+        Vector2 movePos;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentCanvas.transform as RectTransform, Input.mousePosition, parentCanvas.worldCamera, out movePos);
+
+        Vector3 mousePos = parentCanvas.transform.TransformPoint(movePos);
+
+        mouseCursor.transform.position = mousePos;
+
+        transform.position = mousePos;
+    }
+}
