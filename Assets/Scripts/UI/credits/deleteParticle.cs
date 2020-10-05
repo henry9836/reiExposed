e9e3c053b89357a8529d559e7a32cc4495419bf5
Vaughan Webb <vaughanw2012@gmail.com>
2020-10-05 15:54:08 +1300

    its pretty nao

--- Assets/Scripts/UI/credits/deleteParticle.cs
+++ Assets/Scripts/UI/credits/deleteParticle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 
 public class deleteParticle : MonoBehaviour
@@ -9,9 +10,11 @@ public class deleteParticle : MonoBehaviour
 
     private float effectpersent;
     private GameObject effectsref;
+    
 
     void Start()
     {
+        effectsref = GameObject.Find("Scene PostProcess");
         StartCoroutine(effects());
     }
 
@@ -29,30 +32,44 @@ public class deleteParticle : MonoBehaviour
         for (float i = 0.0f; i < 1.0f; i += Time.deltaTime * 10.0f)
         {
             effectpersent = i;
-            effectsref.GetComponent<ChromaticAberration>().intensity.value = effectpersent;
-            effectsref.GetComponent<LensDistortion>().intensity.value = effectpersent * 0.4f;
+            updatevalues(effectpersent);
+
 
             yield return null;
         }
 
         effectpersent = 1.0f;
-        effectsref.GetComponent<ChromaticAberration>().intensity.value = effectpersent;
-        effectsref.GetComponent<LensDistortion>().intensity.value = effectpersent * 0.4f;
+        updatevalues(effectpersent);
+
 
         for (float i = 1.0f; i > 0.0f; i -= Time.deltaTime * 10.0f)
         {
             effectpersent = i;
-            effectsref.GetComponent<ChromaticAberration>().intensity.value = effectpersent;
-            effectsref.GetComponent<LensDistortion>().intensity.value = effectpersent * 0.4f;
+            updatevalues(effectpersent);
+
 
             yield return null;
         }
         effectpersent = 0.0f;
-        effectsref.GetComponent<ChromaticAberration>().intensity.value = effectpersent;
-        effectsref.GetComponent<LensDistortion>().intensity.value = effectpersent * 0.4f;
-
+        updatevalues(effectpersent);
 
         yield return null;
 
+    }
+
+    void updatevalues(float effectammount)
+    {
+
+        ChromaticAberration tmp;
+        if (effectsref.GetComponent<Volume>().profile.TryGet(out tmp))
+        {
+            tmp.intensity.value = effectammount;
+        }
+
+        LensDistortion tmp2;
+        if (effectsref.GetComponent<Volume>().profile.TryGet(out tmp2))
+        {
+            tmp2.intensity.value = effectammount * 0.4f;
+        }
     }
 }
