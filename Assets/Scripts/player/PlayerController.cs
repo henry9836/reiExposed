63be390323110900f49f3bdfd4c593c4c8874363
Vaughan Webb <vaughanw2012@gmail.com>
2020-10-09 17:05:38 +1300

    Auto stash before merge of "patching" and "origin/patching"

--- Assets/Scripts/player/PlayerController.cs
+++ Assets/Scripts/player/PlayerController.cs
@@ -245,6 +245,8 @@ public class PlayerController : MonoBehaviour
 
     public IEnumerator death()
     {
+
+
         deathUI[0].SetActive(true);
 
         for (float i = 0.0f; i < 1.0f; i += Time.deltaTime * 0.4f)
@@ -255,14 +257,15 @@ public class PlayerController : MonoBehaviour
         }
         deathUI[0].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
 
-        yield return new WaitForSeconds(0.5f);
+        float half = SaveSystemController.getFloatValue("MythTraces") / 2.0f;
+        SaveSystemController.updateValue("MythTraces", half);
+        SaveSystemController.saveDataToDisk();
 
-        deathUI[1].SetActive(true);
+        deathUI[5].GetComponent<Text>().text = "You panicked and dropped " + half.ToString() + "¥.\n\nYou blacked out!";
+        deathUI[5].SetActive(true);
 
-        yield return new WaitForSeconds(0.3f);
-        deathUI[1].SetActive(false);
+
         yield return new WaitForSeconds(1.0f);
-
 
         deathUI[2].SetActive(true);
         deathUI[3].SetActive(true);
