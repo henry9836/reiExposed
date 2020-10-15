a933ad10eba65736729802e3b4c199fa4a8b6f70
Vaughan Webb <vaughanw2012@gmail.com>
2020-10-15 14:43:51 +1300

    fixed up main menui

--- Assets/Scripts/UI/main menu/cinematicMenu.cs
+++ Assets/Scripts/UI/main menu/cinematicMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class cinematicMenu : MonoBehaviour
 {
+    public GameObject rotateabout;
     public GameObject target;
     public Camera Cam;
     private float theta = 0.0f;
@@ -13,10 +14,11 @@ public class cinematicMenu : MonoBehaviour
     public float center = 0.0f;
     public Vector3 targetoffset;
 
+
     void Update()
     {
         theta += (Time.deltaTime * speed);
-        Cam.GetComponent<Transform>().localPosition = new Vector3(Mathf.Sin(theta) * circleSize, height, Mathf.Cos(theta) * circleSize);
+        Cam.GetComponent<Transform>().localPosition = new Vector3(Mathf.Sin(theta) * circleSize, height, Mathf.Cos(theta) * circleSize) + rotateabout.transform.position; 
 
         Cam.transform.LookAt(new Vector3(target.transform.localPosition.x + targetoffset.x, target.transform.localPosition.y + targetoffset.y, target.transform.localPosition.z + targetoffset.z), Vector3.up);
         Cam.transform.Rotate(0.0f, center, 0.0f);
