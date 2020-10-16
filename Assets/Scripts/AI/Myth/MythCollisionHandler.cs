2058cf57a2cdd9ff30d53fd4dd6a256bb4f62715
henry9836 <henry983615@gmail.com>
2020-10-16 23:43:06 +1300

    myths can be interrupted on their block and rei moved

--- Assets/Scripts/AI/Myth/MythCollisionHandler.cs
+++ Assets/Scripts/AI/Myth/MythCollisionHandler.cs
@@ -12,7 +12,6 @@ public class MythCollisionHandler : AICollisionHandler
     public List<AudioClip> blockSounds = new List<AudioClip>();
 
     Animator animator;
-    Animator playerAnimator;
     umbrella playerUmbrella;
     Animator playerAnim;
     PlayerController playerCtrl;
@@ -29,8 +28,6 @@ public class MythCollisionHandler : AICollisionHandler
         //Disable AIObjects built-in dectection
         aiObject = GetComponent<AIObject>();
         aiObject.handleCollision = false;
-
-        playerAnimator = aiObject.player.GetComponent<Animator>();
 
         //Get Tracker
         tracker = aiObject.tracker;
@@ -57,27 +54,54 @@ public class MythCollisionHandler : AICollisionHandler
                 //If we can be currently damaged
                 if (playerUmbrella.validDmg(gameObject))
                 {
+
                     //If we are currently blocking and we are facing the player
                     if (animator.GetBool("Blocking") && tracker.isFacingPlayer())
                     {
-
-                        //Stun player and go into attack mode
-                        //Block
-                        playerAnim.SetTrigger("Stun");
-
-                        //Play vfx
-                        Instantiate(blockVFX, playerTransform.position, Quaternion.identity);
-
-                        //Play block sfx
-                        audioSrc.PlayOneShot(blockSounds[Random.Range(0, blockSounds.Count)]);
-
-                        //maybe we will stop blocking maybe we won't :)
-                        //Whack randomly
-                        int coin = Random.Range(0, 11);
-                        if (coin >= 5)
+                        //If we were hit by a heavy attack get stunned and slightly damaged
+                        if (playerAnim.GetBool("HeavyAttack"))
                         {
-                            animator.SetTrigger("Whack");
-                            aiObject.bindAttack("Whack");
+                            float dmg = playerCtrl.umbreallaHeavyDmg * 0.25f;
+
+                            //Deal Damage
+                            aiObject.health -= dmg;
+
+                            //Add onto player known attack
+                            playerUmbrella.targetsTouched.Add(gameObject);
+
+                            //We now know the player's postion so inform tracker
+                            tracker.lastSeenPos = playerTransform.position;
+
+                            //Visible feedback
+                            Instantiate(hitVFX, transform.position, Quaternion.identity);
+                            audioSrc.PlayOneShot(hurtSounds[Random.Range(0, hurtSounds.Count)]);
+                            animator.SetTrigger("Stun");
+
+                            GameObject tmp = GameObject.Instantiate(this.gameObject.GetComponent<AIObject>().damagedText, other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position), Quaternion.identity);
+                            tmp.transform.SetParent(this.transform, true);
+                            tmp.transform.GetChild(0).GetComponent<Text>().text = "-" + dmg.ToString("F0");
+                        }
+                        //We were not hit by a heavy attack
+                        else
+                        {
+                            //Stun player and go into attack mode
+                            //Block
+                            playerAnim.SetTrigger("Stun");
+
+                            //Play vfx
+                            Instantiate(blockVFX, playerTransform.position, Quaternion.identity);
+
+                            //Play block sfx
+                            audioSrc.PlayOneShot(blockSounds[Random.Range(0, blockSounds.Count)]);
+
+                            //maybe we will stop blocking maybe we won't :)
+                            //Whack randomly
+                            int coin = Random.Range(0, 11);
+                            if (coin >= 5)
+                            {
+                                animator.SetTrigger("Whack");
+                                aiObject.bindAttack("Whack");
+                            }
                         }
                     }
                     //We are not blocking
@@ -87,7 +111,7 @@ public class MythCollisionHandler : AICollisionHandler
                         //Get damage value
                         float dmg = playerCtrl.umbreallaDmg;
 
-                        if (playerAnimator.GetBool("HeavyAttack"))
+                        if (playerAnim.GetBool("HeavyAttack"))
                         {
                             dmg = playerCtrl.umbreallaHeavyDmg;
                         }
