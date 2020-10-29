5e5f1c7a20292bc29b3639650144d17cfef7df25
henry9836 <henry983615@gmail.com>
2020-10-29 21:44:32 +1300

    assigned a hide ui key

--- Assets/Scripts/hax.cs
+++ Assets/Scripts/hax.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hax : MonoBehaviour
 {
 
+    public List<Image> uiToHide = new List<Image>();
+    public List<Text> textToHide = new List<Text>();
+
     PlayerController pc;
     AIObject boss;
+    bool uiHide = false;
 
     private void Start()
     {
@@ -14,7 +19,7 @@ public class hax : MonoBehaviour
         boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<AIObject>();
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
         //Health Hax
         if (Input.GetKeyDown(KeyCode.F1))
@@ -44,6 +49,34 @@ public class hax : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F4))
         {
             SaveSystemController.updateValue("MythTraces", 5432);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            uiHide = !uiHide;
+            if (uiHide == false)
+            {
+                for (int i = 0; i < uiToHide.Count; i++)
+                {
+                    uiToHide[i].color = new Color(uiToHide[i].color.r, uiToHide[i].color.g, uiToHide[i].color.b, uiToHide[i].color.a);
+                }
+                for (int i = 0; i < textToHide.Count; i++)
+                {
+                    textToHide[i].color = new Color(textToHide[i].color.r, textToHide[i].color.g, textToHide[i].color.b, textToHide[i].color.a);
+                }
+            }
+        }
+
+        if (uiHide)
+        {
+            for (int i = 0; i < uiToHide.Count; i++)
+            {
+                uiToHide[i].color = new Color(uiToHide[i].color.r, uiToHide[i].color.g, uiToHide[i].color.b, 0.0f);
+            }
+            for (int i = 0; i < textToHide.Count; i++)
+            {
+                textToHide[i].color = new Color(textToHide[i].color.r, textToHide[i].color.g, textToHide[i].color.b, 0.0f);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.F11))
