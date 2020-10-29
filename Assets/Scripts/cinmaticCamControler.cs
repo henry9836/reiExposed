0b1d3c695b5dffe29cc5bb7a9771838b9524b030
Henry <henry983615@gmail.com>
2020-10-29 22:04:03 +1300

    weeee untested a bunch of fixes

--- Assets/Scripts/cinmaticCamControler.cs
+++ Assets/Scripts/cinmaticCamControler.cs
@@ -6,10 +6,42 @@ public class cinmaticCamControler : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            this.gameObject.GetComponent<Animator>().SetTrigger("Play");
+            toggle();
+            this.gameObject.GetComponent<Animator>().SetTrigger("e");
             Debug.Log("playing animation");
         }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            toggle();
+            this.gameObject.GetComponent<Animator>().SetTrigger("calmcluet");
+            Debug.Log("playing animation");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            toggle();
+            this.gameObject.GetComponent<Animator>().SetTrigger("calmdoort");
+            Debug.Log("playing animation");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            toggle();
+            this.gameObject.GetComponent<Animator>().SetTrigger("calmdoorinvtertedt");
+            Debug.Log("playing animation");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            toggle();
+            this.gameObject.GetComponent<Animator>().SetTrigger("calmqrt");
+            Debug.Log("playing animation");
+        }
+    }
+
+
+    private void toggle()
+    {
+         GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = false;
+        this.GetComponent<Camera>().enabled = true;
     }
 }
