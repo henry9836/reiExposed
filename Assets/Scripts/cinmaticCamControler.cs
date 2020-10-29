cc0a5c81c9eb789f0fab77eb513ebffdba218438
Henry <henry983615@gmail.com>
2020-10-29 17:25:27 +1300

    cinematic camera base

--- Assets/Scripts/cinmaticCamControler.cs
+++ Assets/Scripts/cinmaticCamControler.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class cinmaticCamControler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            this.gameObject.GetComponent<Animator>().SetTrigger("Play");
+            Debug.Log("playing animation");
+        }
     }
 }
