958440e8b44d9182c2c4a9ab448739968267cc9d
Henry <henry983615@gmail.com>
2020-10-29 16:45:22 +1300

    timescale mod

---
+++ Assets/Scripts/timescaleMod.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timescaleMod : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Time.timeScale += 0.1f;   
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Time.timeScale -= 0.1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+}
