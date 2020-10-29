58833544cf04fd75a1b7a6cfb72591da5544a641
Henry <henry983615@gmail.com>
2020-10-29 17:01:14 +1300

    poke for h

--- Assets/Scripts/timescaleMod.cs
+++ Assets/Scripts/timescaleMod.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class timescaleMod : MonoBehaviour
 {
-
-
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Time.timeScale += 0.1f;   
-        }
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            Time.timeScale += 0.1f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
             Time.timeScale -= 0.1f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Time.timeScale = 1.0f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
         }
     }
 }
