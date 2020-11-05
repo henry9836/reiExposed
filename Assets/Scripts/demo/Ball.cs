b1185ca846271c579e706ac1c7292f98cf33a754
Vaughan Webb <vaughanw2012@gmail.com>
2020-11-05 14:16:42 +1300

    Auto stash before merge of "demoScene" and "origin/demoScene"

---
+++ Assets/Scripts/demo/Ball.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private float speed;
+    private int currentpos = 0;
+    private List<Vector3> positions = new List<Vector3>() { };
+    private ballmaster BM;
+    private bool stop = false;
+
+    void Start()
+    {
+        BM = this.transform.parent.GetComponent<ballmaster>();
+        positions = BM.positions;
+        speed = BM.speed;
+    }
+
+    void Update()
+    {
+        if (stop == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, positions[currentpos], speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, positions[currentpos]) < 0.1f)
+            {
+                if (currentpos < positions.Count - 1)
+                {
+                    currentpos++;
+                }
+                else
+                {
+                    StartCoroutine(waitabit());
+                }
+            }
+        }
+    }
+
+    public IEnumerator waitabit()
+    {
+        stop = true;
+        BM.stop = true;
+        int tmpsib = this.transform.GetSiblingIndex();
+        if (tmpsib <= 0)
+        {
+            tmpsib = BM.transform.childCount - 1;
+        }
+        else
+        {
+            tmpsib--;
+        }
+
+        GameObject prevBall = BM.transform.GetChild(tmpsib).transform.gameObject; 
+
+
+        while (Vector3.Distance(BM.transform.position, prevBall.transform.position) < BM.spawnDistance)
+        {
+            Debug.Log(Vector3.Distance(BM.transform.position, prevBall.transform.position));
+
+            yield return null;
+        }
+
+        StartCoroutine(disbaleLine());
+
+        currentpos = 0;
+        transform.position = this.transform.parent.position;
+        stop = false;
+
+        yield return null;
+    }
+
+    public IEnumerator disbaleLine()
+    {
+        transform.GetComponent<TrailRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(0.5f);
+        transform.GetComponent<TrailRenderer>().enabled = true;
+
+    }
+}
---
+++ Assets/Scripts/demo/ballmaster.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmaster : MonoBehaviour
+{
+    [Tooltip("add a bunch of gameobjects as childern to the master and their transforms will be used for positions")]
+
+
+    [HideInInspector]
+    public List<Vector3> positions = new List<Vector3>() { };
+    public GameObject ball;
+    public float spawnDistance = 1.0f;
+    public float speed = 1.0f;
+    [HideInInspector]
+    public bool stop = false;
+
+    void Start()
+    {
+        int count = this.transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            positions.Add(transform.GetChild(i).transform.position);
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            Destroy(transform.GetChild(i).transform.gameObject);
+        }
+
+        StartCoroutine(running());
+    }
+
+    public IEnumerator running()
+    {
+
+        GameObject tmp = GameObject.Instantiate(ball);
+        tmp.transform.parent = this.transform;
+
+        while (Vector3.Distance(transform.position, tmp.transform.position) < spawnDistance)
+        {
+            yield return null;
+        } 
+
+        if (stop == false)
+        {
+            StartCoroutine(running());
+        }
+        yield return null;
+
+    }
+}
